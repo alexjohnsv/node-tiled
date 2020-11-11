d935461fb13433efae703d770d6e6f83571c26f3
alexjohnsv <alexjohnsv@gmail.com>
2020-11-11 10:29:21 +0100

    implemented index reference image

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const sharp = require('sharp');
+const path = require('path');
 
 const args = require('minimist')(process.argv.slice(2));
 
@@ -7,6 +8,7 @@ const margin = args.m || 0;
 const fileOut = args.o || 'o/tileset.png';
 const tile_width = args.w || 16;
 const tile_height = args.h || 16;
+
 
 const numberOfFiles = files.length;
 
@@ -29,7 +31,7 @@ const images = files.map((file, index) => {
   }
 })
 
-sharp({
+const image = sharp({
   create: {
     width: w,
     height: h,
@@ -37,5 +39,33 @@ sharp({
     background: { r: 0, g: 0, b: 0, alpha: 0 }
   }
 })
-.composite(images)
-.toFile(fileOut);
+.composite(images);
+
+image.toFile(fileOut);
+
+if (args.r) {
+
+  const images_index_ref = files.map((file, index) => {
+    return {
+      input: Buffer.from(createSvg(index)),
+      left: index * tile_width,
+      top: 0,
+    }
+  });
+
+  const parts = path.parse(fileOut);
+  parts.name = parts.name + '--reference';
+  parts.base = '';
+
+  const fileOutReference = path.format(parts);
+
+  const ref = image
+                .clone()
+                .composite([...images, ...images_index_ref])
+                .toFile(fileOutReference);
+}
+
+
+function createSvg(number) {
+  return `<svg height="${tile_width}" width="${tile_height}"><text x="0" y="15" fill="red">${number}</text></svg>`;
+}
