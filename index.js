2c95c44bb8631e8e93cfa360b70fa608bb1206e0
alexjohnsv <alexjohnsv@gmail.com>
2020-11-15 00:49:42 +0100

    using yargs instead of minimist, to ensure required args and options, better descriptions of usage.

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const sharp = require('sharp');
 const path = require('path');
 
-const args = require('minimist')(process.argv.slice(2));
+const args = require('yargs')(process.argv.slice(2))
+  .usage('Usage: $0 -w resources/1.png resources/2.png -w [num] -h [num] -o [output file]')
+  .demandCommand(1)
+  .demandOption(['w', 'h', 'o'])
+  .default('m', 0)
+  .describe('m', 'Sets the margin between tiles')
+  .describe('w', 'Tile width')
+  .describe('h', 'Tile height')
+  .describe('o', 'Output file')
+  .option('r')
+  .describe('r', 'Create a numbered reference image')
+  .default('r', false)
+  .boolean('r')
+  .argv;
 
 const files = args._;
-const margin = args.m || 0;
-const fileOut = args.o || 'o/tileset.png';
-const tile_width = args.w || 16;
-const tile_height = args.h || 16;
-
+const margin = args.m;
+const fileOut = args.o;
+const tile_width = args.w;
+const tile_height = args.h;
 
 const numberOfFiles = files.length;
-
-if (numberOfFiles === 0) {
-  return;
-}
 
 const w = (numberOfFiles * tile_width) + (margin * (numberOfFiles - 1));
 const h = tile_height
